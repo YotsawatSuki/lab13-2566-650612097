e32ce15727114aee34245eb6c344c341c453fb64
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-08-07 15:42:23 +0700

    finished

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Yotsawat Sukinee"
+            studentId="650612097"
           />
         </div>
       </body>
